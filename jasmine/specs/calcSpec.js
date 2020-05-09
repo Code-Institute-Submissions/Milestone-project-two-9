2cb47c60a82de261fa1ac2333357b013f396e1d4
OGMyst <hughwiliampadmore@hotmail.co.uk>
2020-05-09 15:56:55 +0000

    Started working on jasmine testing

---
+++ jasmine/specs/calcSpec.js
@@ -0,0 +1,7 @@
+describe("randomiser", function(){
+    describe("cards", function(){
+        it("should return a string", function(){
+            expect(cards("hearts").toBe("hearts"));
+        });
+    });
+});
