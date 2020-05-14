e4cc7cb0abbf5bc6f3a2f1158b39f055be7430b0
OGMyst <hughwiliampadmore@hotmail.co.uk>
2020-05-14 15:39:51 +0000

    fixed issue where all carads would flip at once, changed title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,3 @@
-$(document).ready(function () {
-        $(".card-back").click(function(){
-            $(this).removeClass("card-back");
-            $("div").prev().css("opacity", "1")
-
-        });
-});
-
 const cardValue =  ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
 const cardSuit = ["hearts", "spades", "clubs", "diamonds"];
@@ -14,7 +6,7 @@ function cards(){
 
     let x;
 
-    for (x = 0; x < 1; x++){ //to iterate through each element with the selected classes
+    for (x = 0; x < 2; x++){ //to iterate through each element with the selected classes
 
     //gets a random value from cardValue
     let j = Math.floor(Math.random()*(13 - 0)) + 0;
@@ -33,3 +25,9 @@ function cards(){
     cSuit[x].innerHTML = `<img class="card-image" src="assets/images/${cardType}.svg.png" alt="${cardType}"/>`;
 }}
 
+$(document).ready(function () {
+    $(".card-back").click(function(){
+        $(this).removeClass("card-back");
+        $(this).prev().css({"opacity": "1"});
+    });
+});
