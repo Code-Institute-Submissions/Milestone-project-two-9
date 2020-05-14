9dd529cbc0afefc4c3f70416fad21a6c6e1d8e8b
OGMyst <hughwiliampadmore@hotmail.co.uk>
2020-05-14 15:01:32 +0000

    Added click command to flip cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+$(document).ready(function () {
+        $(".card-back").click(function(){
+            $(this).removeClass("card-back");
+            $("div").prev().css("opacity", "1")
+
+        });
+});
 
 const cardValue =  ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
 
@@ -7,7 +14,7 @@ function cards(){
 
     let x;
 
-    for (x = 0; x < 99; x++){ //to iterate through each element with the selected classes
+    for (x = 0; x < 1; x++){ //to iterate through each element with the selected classes
 
     //gets a random value from cardValue
     let j = Math.floor(Math.random()*(13 - 0)) + 0;
@@ -26,11 +33,3 @@ function cards(){
     cSuit[x].innerHTML = `<img class="card-image" src="assets/images/${cardType}.svg.png" alt="${cardType}"/>`;
 }}
 
-$(document).ready(function () {
-    function cardSwitch(){
-        $("card-back").click(function(){
-            $(this).removeClass("card-back");
-            $(this).css("opacity", "1");
-        });
-    }
-});
