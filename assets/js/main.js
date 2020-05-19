899c231eab0036cf2ccd094459a8225e1cded018
OGMyst <hughwiliampadmore@hotmail.co.uk>
2020-05-19 12:57:16 +0000

    imported files from 'OGMyst/Jasmine-testingMS2'. Updated unit test descriptions

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,16 @@ const cardSuit = ["hearts", "spades", "clubs", "diamonds"];
 
 function cards(){
 
+    //Creates a multidimensional array filled with a two of each combined value of cardValue and cardSuit
     let orderedCards = [];
-    while (orderedCards.length < 32){ //to iterate through each element with the selected classes
+    while (orderedCards.length < 32){ 
         let j = Math.floor((Math.random()*13) + 0);
         let i = Math.floor((Math.random()*3) + 0);
         cardFinal = [cardValue[j], cardSuit[i]] 
         orderedCards.push(cardFinal, cardFinal);  
     }
 
+    //Creates an array of unordered numbers to be used as index numbers for orderedCards array
     let cardShuffling = [];
     while (cardShuffling.length < 32){
         let orderNumber = Math.floor((Math.random()*32) + 0);
@@ -21,15 +23,12 @@ function cards(){
         }
     }
 
+    //inputs a value of a complete playing card to the HTML 
     let x;
-
-    for (x = 0; x < 32; x++){ //to iterate through each element with the selected classes
+    for (x = 0; x < 32; x++){
         let i = cardShuffling[x];
-        //inputs cardValue as the card number
         let cNumb = document.getElementsByClassName("number-text");
         cNumb[x].innerHTML = `${orderedCards[i][0]}`;
-
-        //uses cardSuit value for image src
         cSuit = document.getElementsByClassName("card-suit");
         cSuit[x].innerHTML = `<img class="card-image" src="assets/images/${orderedCards[i][1]}.svg.png" alt="${orderedCards[i][1]}"/>`;
             
---
+++ jasmine/specs/calcSpec.js
@@ -0,0 +1,102 @@
+const cardValue =  ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+
+const cardSuit = ["Hearts", "Spades", "Clubs", "Diamonds"];
+
+describe("randomiser", function(){
+
+    // test one
+    it("should return a number", function(){
+        // arrange
+        let indexnum = 2;
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(2);
+    });
+
+    //test two
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(1,9));
+        let indexnum = isFinite(i)
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
+
+    //checks specific value is being returned from cardValue
+    it("should return 3", function(){
+        // arrange
+        let cardname = cardValue[2];
+        let indexnum = cardname;
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe("3");
+    });
+
+    //checks any value from cardValue can be called and has correct data type
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(0,12));
+        let cardname = cardValue[i];
+        let indexnum = isNaN(cardname);
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
+
+    //checks specific value is being returned from cardSuit
+    it("should return Hearts", function(){
+        // arrange
+        let cardname = cardSuit[0];
+        let indexnum = cardname;
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe("Hearts");
+    });
+
+    //checks any value from cardSuit can be called and has correct data type
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(0,3));
+        let cardname = cardSuit[i];
+        let indexnum = isNaN(cardname);
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
+
+    //checks specific values are being called and being combined in the correct way
+    it("should return AofHearts ", function(){
+        // arrange
+        let cardType = cardSuit[0];
+        let cardname = cardValue[0];
+        let indexnum = cardname + "of"+ cardType;
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe("AofHearts");
+    });
+
+    //checks any value from cardSuit and cardValue can be called and has correct data type
+    it("should return true", function(){
+        // arrange
+        let i = Math.floor(Math.random(0,3));
+        let cardType = cardSuit[i];
+
+        let j = Math.floor(Math.random(0,12));
+        let cardname = cardValue[j];
+
+        let indexnum = isNaN (cardname + "of"+ cardType);
+        // act
+        const result = cards(indexnum);
+        //assert
+        expect(result).toBe(true);
+    });
+});
+
